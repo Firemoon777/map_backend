a0911370dd46b79d12c990ca766d48a296f7cd5a
Firemoon777 <firemoon@icloud.com>
2018-06-05 07:59:13 +0000

    api server with static data implemented

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # Магия, не трогать!
 
+## Объекты
+```
+type Station struct {
+	id	int64
+        name    []string
+        color   []int
+        x       float32
+        y       float32
+        active  []bool
+}
+
+type Event struct {
+        id      int             
+        name    string          
+        time    int
+	stations []int
+}
+```
+
+## Методы
+
+`GET /events/` -- Возвращает все события и список станций, доступных на момент события
+`GET /station/{id}` -- Возвращает подробную информацию о станции с id = {id}
+
+
+
---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"encoding/json"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+type Station struct {
+	Id	int64		`json:"id"`
+	Name 	[]string	`json:"name"`
+	Color 	[]int		`json:"color"`
+	X 	float32		`json:"x"`
+	Y 	float32		`json:"y"`
+	Active 	[]bool		`json:"active"`
+}
+
+type Event struct {
+	Id	int		`json:"id"`
+	Name	string		`json:"name"`
+	Time	int		`json:"timestamp"`
+	Stations []int 		`json:"stations"`
+}
+
+type Events []Event
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/events/", EventHandler)
+	router.HandleFunc("/station/{stationId}", StationHandler)
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func EventHandler(w http.ResponseWriter, r *http.Request) {
+	events := Events{
+		Event{Name: "Открытие станций первой очереди", Stations: []int{1}},
+		Event{Name: "Открытие Пушкинской", Stations: []int{1, 5}},
+	}
+	json.NewEncoder(w).Encode(events)
+}
+
+func StationHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.ParseInt(vars["stationId"], 10, 64)
+	if err != nil {
+		fmt.Fprintln(w, err)
+		return
+	} 
+
+	station := Station{Id: id, Name: []string{"Обводной канал", "Каретная"}, X: float32(5.0*id), Y: float32(5.0*id), Active: []bool{true, false}, Color: []int{0xFF0000, 0x654321}}
+	json.NewEncoder(w).Encode(station)
+}
